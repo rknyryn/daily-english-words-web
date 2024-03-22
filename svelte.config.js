a25dcf60c770696d8abee2cddee0c721ed27de0f
Kaan Yarayan <rknyryn@gmail.com>
2024-03-22 11:49:43 +0300

    configurations updated for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -10,19 +10,22 @@ const config = {
 	kit: {
 		alias: {
 			'@/*': './path/to/lib/*',
-			"@components":"./src/components",
-			"@assets":"./src/assets",
-			"@api":"./src/api",
-			"@lib":"./src/lib",
-			"@utils":"./src/utils",
+			'@components': './src/components',
+			'@assets': './src/assets',
+			'@api': './src/api',
+			'@lib': './src/lib',
+			'@utils': './src/utils'
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages:"build",
-			assets:"static"
-		})
+			pages: 'build',
+			assets: 'static'
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/daily-english-words-web.github.io' : ''
+		}
 	}
 };
 
