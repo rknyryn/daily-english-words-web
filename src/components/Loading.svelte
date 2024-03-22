13e4b7ddeef5eabcd13502ad0f9c79ff6b24cc8b
Kaan Yarayan <rknyryn@gmail.com>
2024-03-22 12:30:34 +0300

    img src path changed

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
 <div class="fixed left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-row items-end gap-2">
-	<img src="/loading-two.gif" style="width: 90px; height: 90px;" draggable="false" alt="" />
+	<img src="{base}/loading-two.gif" style="width: 90px; height: 90px;" draggable="false" alt="" />
 </div>
