c8fb94185c00a3f4a89e2a2d4fd433c3defe789d
kerimcanbalkan <kerimcanbalkan@gmail.com>
2024-03-23 22:36:11 +0300

    set default colorscheme to system

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import '../app.pcss';
 	import Navbar from '@components/Navbar.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
+<ModeWatcher />
 <Navbar />
 
 <main class="container px-0">
